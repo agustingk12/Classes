b44bab8f9c9e0aa6f44218b903095069948aa5c7
agustingk12 <128102716+agustingk12@users.noreply.github.com>
2023-04-10 18:54:46 -0300

    Update ejercicio1.js
    Fix syntaxis of function, added function call

--- ClaseJs/ejercicio1.js
+++ ClaseJs/ejercicio1.js
@@ -1,13 +1,16 @@
-let lowerLimitEven = 1
+let lowerLimitEven = 2
 let upperLimitEven = 100
 let lowerLimitOdd = 1
-let upperLimitOdd = 100
+let upperLimitOdd = 99
 
-function checkRange ()
+function checkRange(){      
+  for (let number = lowerLimitOdd || lowerLimitEven ; number <= upperLimitOdd || number <= upperLimitEven ; number++){
+      if(number % 2 == 0){
+        console.log(`El numero ${number} es par`)
+      } else {
+        console.log(`El numero ${number} es impar`)
+      }
+  }
+}
 
-for (lowerLimitOdd ; lowerLimitOdd <= upperLimitOdd; lowerLimitOdd++){
-      if(lowerLimitOdd % 2 == 0){
-        console.log(`El numero ${lowerLimitOdd} es par`)
-        }
-            console.log(`El numero ${lowerLimitOdd} es impar`)
-    }
+checkRange()
