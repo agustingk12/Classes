dc318351b5ad4ad147ba826302de12d5e9b5496a
Agustín Gercek <gercekagustin14@gmail.com>
2023-04-12 19:57:38 -0300

    Fix answer

--- clase12/ejercicio2.js
+++ clase12/ejercicio2.js
@@ -1,12 +1,11 @@
 const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 const array2 = [1, 2, 3, 4]
+const array3 = ['perro', 'gato', 'zorro', 10, 20, 30, 'elefante']
 
 const checkLengthDelLast = (a) => { 
   if (a.length > 5){
     console.log(a.slice(0,-3))
 }
-  else {console.log('error: array', +${a}, 'not long enough')}
+  else {console.log('error: array not long enough')}
 }
-checkLengthDelLast(array2)
-
-console.log(array.length)
+checkLengthDelLast(array3)
